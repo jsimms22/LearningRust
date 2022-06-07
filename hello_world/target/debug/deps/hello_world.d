62c5694deabe3d6d70cd18310c255fa0436cb0d1
jsimms22 <jpsimms2057@gmail.com>
2022-06-06 22:09:23 -0500

    testing gitignores and rm command

--- hello_world/target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-C:\Code\LearningRust\hello_world\target\debug\deps\hello_world.exe: src\main.rs
-
-C:\Code\LearningRust\hello_world\target\debug\deps\hello_world.d: src\main.rs
-
-src\main.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-C:\Code\LearningRust\hello_world\target\debug\hello_world.exe: C:\Code\LearningRust\hello_world\src\main.rs
