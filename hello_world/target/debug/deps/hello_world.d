c6a9df0851933072b46722b0daa4fb9eff38ea4e
jsimms22 <jpsimms2057@gmail.com>
2022-06-06 22:14:10 -0500

    removed gitignores

---
+++ hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Code\LearningRust\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Code\LearningRust\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Code\LearningRust\hello_world\target\debug\hello_world.exe: C:\Code\LearningRust\hello_world\src\main.rs
