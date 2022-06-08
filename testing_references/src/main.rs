a94ab285c88ce1a442664936709287fadcb3557b
jsimms22 <jpsimms2057@gmail.com>
2022-06-07 23:47:03 -0500

    added example code of dangling references

--- testing_references/src/main.rs
+++ testing_references/src/main.rs
@@ -9,3 +9,27 @@ fn main() {
     println!("printing 's': {}", s_ptr);
     println!("printing '&s': {}", &s_ptr);
 }
+
+//the following code does not work
+fn main() {
+    let reference_to_nothing = dangle();
+}
+    
+fn dangle() -> &String { // dangle returns a reference to a String
+    let s = String::from("hello"); // s is a new String
+    
+    &s // we return a reference to the String, s
+} // Here, s goes out of scope, and is dropped. Its memory goes away.
+  // Danger!
+
+
+// compilable version of the above code
+fn main() {
+    let reference_to_nothing = dangle();
+}
+    
+fn dangle() -> &String { // dangle returns a reference to a String
+    let s = String::from("hello"); // s is a new String
+    
+    s // we return the String, s
+} 
